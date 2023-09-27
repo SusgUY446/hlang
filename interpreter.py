d6c30e843e853d54ba3761bbe01ec65a3dae8c91
SusgUY446 <129160115+SusgUY446@users.noreply.github.com>
2023-09-27 10:47:39 +0200

    Update interpreter.py

--- interpreter.py
+++ interpreter.py
@@ -1,7 +1,14 @@
-import os
-import time
 
-class main:
-  def __init__(self):
+user_input = input()
+
+words = user_input.split()
+
+if words[0] == "say":
+    if len(words) >= 2:
+        temp_storage = words[1]
+        print(f"{temp_storage}")
+    else:
+        print("")
+else:
+  if words[0] == "stop":
     pass
-  
